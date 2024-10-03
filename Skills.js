beb532dadcbfa3cb00e737b608708ded84a7c0fe
Chaitanya Mahmuni <78221851+chai0371@users.noreply.github.com>
2024-10-03 17:53:42 +0530

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>Java, C, Embedded C, Arduino</li>
+        <li>React.js, Node.js, PHP, MySQL</li>
+        <li>IoT, Microcontrollers, Bluetooth Integration</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
