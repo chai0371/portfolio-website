c5b16555781ce6515f94becc03afa371e1359d28
Chaitanya Mahmuni <78221851+chai0371@users.noreply.github.com>
2024-10-03 17:53:02 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am currently pursuing B.Tech in Electronics and Telecommunication at Vishwakarma Institute of Information Technology. I have hands-on experience in embedded systems, IoT, and web development. I am passionate about building projects that solve real-world problems.
+      </p>
+    </section>
+  );
+}
+
+export default About;
