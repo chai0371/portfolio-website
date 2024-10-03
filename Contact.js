a9344c2808b8ea8a2fba4e5c317c22ae1d057d2c
Chaitanya Mahmuni <78221851+chai0371@users.noreply.github.com>
2024-10-03 17:54:03 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: chaitanya.22320022@viit.ac.in</p>
+      <p>GitHub: <a href="https://github.com/chai0371" target="_blank">chai0371</a></p>
+      <p>LinkedIn: <a href="https://linkedin.com/in/chaitanyamahamuni" target="_blank">Chaitanya Mahamuni</a></p>
+    </section>
+  );
+}
+
+export default Contact;
