57ff685b0bc064bcf2307924e430ee99bcc32ceb
Chaitanya Mahmuni <78221851+chai0371@users.noreply.github.com>
2024-10-03 17:52:41 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Chaitanya Mahamuni</h1>
+      <p>Electronics & Telecommunication Engineer | Web Developer | IoT Enthusiast</p>
+    </header>
+  );
+}
+
+export default Header;
